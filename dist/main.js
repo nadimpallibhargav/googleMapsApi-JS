94a8fb091b572b963d94123597d35fafda9759e2
bhargav27b <bhargav@sketchbrahma.com>
2022-07-13 16:35:51 +0530

    adding markers with content

--- dist/main.js
+++ dist/main.js
@@ -11,15 +11,29 @@ function initMap() {
     // map
     let mainMap = new google.maps.Map(mapFrame, options);
 
-    function addMarker(mapProperties) {        
+
+    function addMarker(mapProperties) {     
         let marker = new google.maps.Marker({
             position: mapProperties.cordinates,
             map: mainMap,
             icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
         });
+
+        if(mapProperties.content) {
+            let markerText = new google.maps.InfoWindow({
+                content: mapProperties.content
+            });
+
+            marker.addListener('click', () => {
+                markerText.open(map,marker);
+            });
+        }
     }
 
-    addMarker({cordinates: {lat:13.0332221,lng:77.6299528}});
-    addMarker({cordinates: {lat:13.0332221,lng:77.6299800}});
+
+    addMarker({cordinates: {lat:13.0332221,lng:77.6299528}, content: '<h3>First place to visit</h3>'});
+    addMarker({cordinates: {lat:13.0332221,lng:77.6299800}, content: '<h3>Second place to visit</h3>'});
+
+
 
 }
